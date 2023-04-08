a4ab5ef15a74f0638c6308ba36af2e8f810062bf
marlonbarrios <marlon@dance-tech.net>
2023-04-07 20:52:28 -0400

    alan v1

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Head>
-        <title>Sati-AI</title>
+        <title>Alan-AI</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className='px-4 md:px-0'>{children}</main>
